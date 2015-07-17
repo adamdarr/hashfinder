f27f6654a69a5bffe7447a6320851abd2d4f11e1
adamdarr <adamdarr@gmail.com>
2015-07-17 19:02:39 -0400

    no message

--- hashfinder.py
+++ hashfinder.py
@@ -1,8 +1,8 @@
 import hashlib
 import sys
 
-goal = ("f04031af5ee9939a931a062b836c385eb2ee681fd704ed919534b78329ae34e173"
-        "e13c31ebfd03db1eb9e9759cd3365195d0d7d53028256038b24975ace8c83d")
+goal = ("f04031af5ee9939a931a062b836c385eb2ee681fd704ed919534b78329ae34e1"
+        "73e13c31ebfd03db1eb9e9759cd3365195d0d7d53028256038b24975ace8c83d")
 
 for i in range(256):
     for j in range(256):
